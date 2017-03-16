e8301a9e70a723dd429a82bf0d0bf8b766aa2c52
王睿龙 <832452159@qq.com>
2017-03-16 17:34:58 +0800

    设置html编译

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,7 +1,15 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry:'./src/entry.js',
     output:{
         path:'/',
         filename:'bundle.js'
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'index.html',
+            inject:true
+        })
+    ]
 }
