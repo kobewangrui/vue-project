a49c8f20c720a0f85ca59a947f41215cd4d1dd0c
王睿龙 <832452159@qq.com>
2017-03-16 17:16:37 +0800

    设置webpack编译规则

--- build/dev-server.js
+++ build/dev-server.js
@@ -3,6 +3,17 @@ let opn = require('opn')
 let webpack = require('webpack')
 
 let app = express()
+
+
+let compiler = webpack({
+    entry:'./src/entry.js',
+    output:{
+        path:'/',
+        filename:'bundle.js'
+    }
+})
+
+
 app.listen(9999,function(){
     opn("http://localhost:9999")
     console.log("listen at localhost:9999")
