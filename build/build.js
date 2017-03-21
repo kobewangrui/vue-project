4bd574a892a56b794600bad0929bc6869396dd21
王睿龙 <832452159@qq.com>
2017-03-21 17:21:57 +0800

    设置编译后文件输出路径

--- build/build.js
+++ build/build.js
@@ -1,6 +1,2 @@
-// 设置node为生产模式
-IDBEnvironment.NODE_ENV = 'production'
-
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
-webpack(webpackConfig,function(err,stats){})
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,2 +1,15 @@
 var webpack = require('webpack')
+var baseWebpackConfig = require('./webpack.base.conf')
 var merge = require('webpack-merge')
+var path = require('path')
+
+
+var webpackConfig = merge(baseWebpackConfig,{
+    output:{
+        path:path.resolve(__dirname,'../dist'),
+        filename:'js/bundle.js'
+    }
+})
+
+// 设置node为生产模式
+// IDBEnvironment.NODE_ENV = 'production'
