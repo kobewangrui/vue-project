765c2ca86d755f53c80077a354ae3d813f352c18
王睿龙 <832452159@qq.com>
2017-03-21 18:14:17 +0800

    引入vue-resource

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Router from './config/router'
-
+import Resource from 'vue-resource'
 
 Vue.use(VueRouter)
 var router = Router(VueRouter)
