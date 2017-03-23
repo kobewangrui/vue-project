d8376ccf9eb81807921c1166c9dea81924613422
王睿龙 <832452159@qq.com>
2017-03-23 17:49:44 +0800

    配置生产环境静态文件处理

--- build/build.js
+++ build/build.js
@@ -1,3 +1,9 @@
+requre('shelljs/global')
+
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
+var assetsPath = './dist/static'
+mkdir('-p',assetsPath)
+cp('-R','static/*',assetsPath)
+
 webpack(webpackConfig,function(err,stats){})
