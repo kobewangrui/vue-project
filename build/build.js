4f21559bdaec6f79fa2ff9821df8da982df892f8
王睿龙 <832452159@qq.com>
2017-03-23 18:21:01 +0800

    添加注释

--- build/build.js
+++ build/build.js
@@ -3,8 +3,11 @@ requre('shelljs/global')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 var assetsPath = './dist/static/'
+//删除老的dist文件夹
 rm('-rf','./dist')
+// 生成新的dist
 mkdir('-p',assetsPath)
+// cope生成dist到static
 cp('-R','static/*',assetsPath)
 
 webpack(webpackConfig,function(err,stats){})
