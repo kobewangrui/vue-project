4fddb19e6d567c6d4cd9929421b330bb34765cb4
王睿龙 <832452159@qq.com>
2017-03-28 17:37:00 +0800

    添加样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,11 @@
-<style scoped></style>
+<style>
+    body{
+        background:gray;
+    }
+</style>
 <template>
     <div>
         <img src="http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg" alt="">
-        1111
     </div>
 </template>
 <script></script>
